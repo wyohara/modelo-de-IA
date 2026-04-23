5d5b34c79e69afd345fab763baf159f6fb81ad39
wyohara <wyohara.dev@gmail.com>
2026-04-23 07:28:09 -0300

    criado o savepoint

--- src/modelo/tokenizador/modelos_processamentos/processador_bpe.py
+++ src/modelo/tokenizador/modelos_processamentos/processador_bpe.py
@@ -9,8 +9,9 @@ from src.ferramentas.ferramentas import texto_para_hex, hex_para_texto
 class Processador_BPE:
     def __init__(self):
         self.__lista_tokens = Path('src/media/dados_processados/tokens_bpe.csv')
+        self.__savepoint = Path('src/media/dados_processados/savepoint_bpe.csv')
 
-    def __contar_caracteres_texto(self, path:Path)->defaultdict:
+    def __contar_caracteres(self, path:Path)->defaultdict:
         '''
         Método que conta os caracteres do texto e salva em um default dict no formato: {chave:valor}
             Params:
@@ -26,10 +27,6 @@ class Processador_BPE:
                     lista_bpe[i]+=1
                 except KeyError:
                     lista_bpe[i]=1
-
-        #converte o dicionário em lista [(chave,valor)] para salvar em csv
-        caracteres = list(lista_bpe.items())
-        self.__salvar_tokens_csv(caracteres)
         return lista_bpe
 
     def __achar_caractere_coringa(self, lista_char:defaultdict)->str:
@@ -91,7 +88,7 @@ class Processador_BPE:
             Params:
                 dados:list = lista de dados a ser salvo no csv no formato [(chave, valor)]
         '''
-        df = self.__carregar_tokens_csv()
+        df = pd.DataFrame(columns=['valor'])
         #separa a coluna valor
         coluna_valor = df.columns[0]
         #itera os dados para salvar os valores
@@ -108,6 +105,23 @@ class Processador_BPE:
         df_ordenado = df.sort_values(by=df.columns[0], ascending=False)
         df_ordenado.to_csv(str(self.__lista_tokens))
 
+    def __unir_dicionarios(self, dict_adicional:dict, dict_saida:dict)->dict:
+        """
+        Método acessório que une o dicionario adicional ao dicionário de saída
+            Params:
+                dict_adicional:dict = dicionário adicional que será unido
+                dict_saida:dict = dicionário de saída que recebe os dados
+            Return:
+                dict = valor do dicionário de saída
+        """        
+        for chave_1 in dict_adicional.keys():
+            if chave_1 in dict_saida.keys():
+                dict_saida[chave_1] += dict_adicional[chave_1]
+            else:
+                dict_saida[chave_1] = dict_adicional[chave_1]
+        del dict_adicional
+        return dict_saida
+
     def processar_texto(self, path:Path, quantidade=150000):
         '''
         Método principal do processador de texto. Percorre a quantidade de vezes para fazer o BPE
@@ -115,27 +129,63 @@ class Processador_BPE:
                 path:Path = Caminho até o arquivo
                 quantidade: int = número de iterações para gerar os tokens. Padrão 150000
         '''
-        caracteres = self.__contar_caracteres_texto(path)
-        coringa = self.__achar_caractere_coringa(caracteres)
+        dict_char = self.__contar_caracteres(path)
+        coringa = self.__achar_caractere_coringa(dict_char)
+
+        #carregando o csv
         df = self.__carregar_tokens_csv()
         df = df.reset_index()
         lista_bpe = defaultdict(int, df.values.tolist())
+
+        pos_index, tk_set = self.__carregar_savepoint()
+        if pos_index == -1:
+            lista_bpe = self.__unir_dicionarios(dict_char, lista_bpe)
         
-        set_processados = set()
         # faz um loop até o total de iterações solicitado +1 poi começa de 1, não 0
         for i in range(1,quantidade+1):
-            #carrega o csv e ordena
-            chaves_ordenadas = sorted(lista_bpe, key=lista_bpe.get, reverse=True)
-            # se não foi processado aplica o BPE
-            for char_chave in chaves_ordenadas:
-                if char_chave not in set_processados:
-                    percentil = quantidade/100.0          
-                    if int(i%percentil) ==0:
-                        print(f"\t>>> Processado {(i/quantidade)*100:.2f}% do texto {path.name}: {i} de {quantidade}")
-                    lista_bpe = self.__aplicar_bpe(lista_bpe, path, char_chave, coringa)
-                    set_processados.add(char_chave)
-                    break
+            if i> pos_index:
+                #carrega o csv e ordena
+                chaves_ordenadas = sorted(lista_bpe, key=lista_bpe.get, reverse=True)
+                # se não foi processado aplica o BPE
+                for char_chave in chaves_ordenadas:
+                    if char_chave not in tk_set:
+                        percentil = quantidade/100.0          
+                        if int(i%percentil) ==0:
+                            print(f"\t>>> Processado {(i/quantidade)*100:.2f}% do texto {path.name}: {i} de {quantidade}")
+                            self.__salvar_tokens_csv(list(lista_bpe.items()))
+                            self.__marcar_savepoint(i, tk_set)
+                        lista_bpe = self.__aplicar_bpe(lista_bpe, path, char_chave, coringa)
+                        tk_set.add(char_chave)
+                        break
         
         #salva os dados como csv
+        self.__marcar_savepoint(-1,set())
         self.__salvar_tokens_csv(list(lista_bpe.items()))
-        set_processados = set()
+        tk_set = set()
+    
+    def __marcar_savepoint(self, pos, setlist):
+        try:
+            df = pd.read_csv(str(self.__savepoint),index_col=0)
+        except Exception as e:
+            df = pd.DataFrame(columns=['valor'])
+        coluna_valor = df.columns[0]
+        df.index.name = 'chave'
+        #itera os dados para salvar os valores
+        df.loc['setlist', coluna_valor] = str(set(setlist))
+        df.loc['pos', coluna_valor] = str(pos)
+        df.to_csv(str(self.__savepoint))
+    
+    def __carregar_savepoint(self)-> tuple[int, set]:
+        try:
+            df = pd.read_csv(str(self.__savepoint),index_col=0)
+            coluna_valor = df.columns[0]
+            pos_str = df.loc['pos', coluna_valor]
+            setlist_str = df.loc['setlist', coluna_valor]
+            return int(pos_str), ast.literal_eval(setlist_str)
+        except FileNotFoundError as e:
+            return -1, set()
+
+    def get_tokens(self):
+        df = self.__carregar_tokens_csv()
+        df.insert(0, 'id', range(1, len(df) + 1))
+        return df.sort_values('valor', ascending=False)
---
+++ src/modelo/tokenizador/tokenizador.py
@@ -0,0 +1,14 @@
+
+
+class Tokenizador():
+    def __init__(self):
+        pass
+
+    def gerar_tokenizador(self, quantidade=120000):
+        pass
+
+    def tokenizar(self, texto:str)->list[int]:
+        pass
+
+    def reverter(self, lista_tokens:list[int], concatenar:False)->str:
+        pass
